cda059aa16a54d6ff9d879337041ffbb74d512f5
DESKTOP <JP4>
2025-09-01 23:58:33 +0800

    added test chart

--- Models/StackChartData.cs
+++ Models/StackChartData.cs
@@ -7,6 +7,10 @@ namespace ChartAPI.Models
         public string Name { get; set; }
         //public string Title { get; set; }
 
+        private string _groupName = "WorkNo";
+        private string _Regular;
+        private string _Overtime;
+
         public string[] AxisTitle { get; set; }
         //public string YAxisName { get; set; }
         //public string[] LegendData { get; set; }
@@ -16,7 +20,6 @@ namespace ChartAPI.Models
             this.Name = monthGroup.Key + "月";
             this.AxisTitle = monthGroup
                 .GroupByProperty(groupName)
-                //.GroupBy(x => x.WorkNo)
                 .OrderBy(dg => dg.Key)
                 .Select(workNoGroup => workNoGroup.Key.ToString())
                 .ToArray();
@@ -24,12 +27,13 @@ namespace ChartAPI.Models
                 {
                     new Series("Regular")
                     {
-                        Values = monthGroup.GroupBy(x => x.WorkNo).OrderBy(g => g.Key)
+                        //Values = monthGroup.GroupBy(x => x.WorkNo).OrderBy(g => g.Key)
+                        Values = monthGroup.GroupByProperty(groupName).OrderBy(g => g.Key)
                         .Select(g => g.Where(x => x.Regular).Sum(x => x.Hours)).ToArray()
                     },
                     new Series("Overtime")
                     {
-                        Values = monthGroup.GroupBy(x => x.WorkNo).OrderBy(g => g.Key)
+                        Values = monthGroup.GroupByProperty(groupName).OrderBy(g => g.Key)
                         .Select(g => g.Where(x => x.Overtime).Sum(x => x.Hours)).ToArray()
                     }
                 };
