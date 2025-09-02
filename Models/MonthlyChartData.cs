a275d1850b1e5358af618391b7e51aea54e4334c
DESKTOP <JP4>
2025-09-03 00:27:04 +0800

    added GetStackChartDto API

--- Models/MonthlyChartData.cs
+++ Models/MonthlyChartData.cs
@@ -6,9 +6,6 @@ namespace ChartAPI.Models
     {
         public int Month { get; set; } // 1~12
         public Dictionary<string, PieChartData> PieChartDic { get; set; }
-        public StackChartData StackChart { get; set; }
-        //public PieChartData PieChart { get; set; }
-        //public PieChartData PieChart2 { get; set; }
-
+        public StackChartData<ManHourModel> StackCharts { get; set; }
     }
 }
--- Models/StackChartData.cs
+++ Models/StackChartData.cs
@@ -1,47 +1,71 @@
 ﻿using ChartAPI.Extensions;
+using System.Reflection;
 
 namespace ChartAPI.Models
 {
-    public class StackChartData
+    public class StackChartData<T>
     {
         public string Name { get; set; }
-        //public string Title { get; set; }
-
-        private string _groupName = "WorkNo";
-        private string _Regular;
-        private string _Overtime;
-
         public string[] AxisTitle { get; set; }
-        //public string YAxisName { get; set; }
-        //public string[] LegendData { get; set; }
-        public List<Series> Series { get; set; } // 每個 bar 類別
-        public StackChartData(IGrouping<int, ManHourModel> monthGroup, string name, string groupName)
+        public List<Series> Series { get; set; }
+
+        private IEnumerable<T> _sourceData;
+        private string _groupName;
+        private List<StackSeries> _stackSeries;
+        // 每個 bar 類別
+        public StackChartData(IEnumerable<T> source, string groupName, List<StackSeries> stackSeries)
         {
-            this.Name = monthGroup.Key + "月";
-            this.AxisTitle = monthGroup
+            _sourceData = source;
+            _groupName = groupName;
+            _stackSeries = stackSeries;
+
+            this.AxisTitle = _sourceData
                 .GroupByProperty(groupName)
-                .OrderBy(dg => dg.Key)
+                .OrderBy(g => g.Key)
                 .Select(workNoGroup => workNoGroup.Key.ToString())
                 .ToArray();
-            this.Series = new List<Series>()
-                {
-                    new Series("Regular")
-                    {
-                        //Values = monthGroup.GroupBy(x => x.WorkNo).OrderBy(g => g.Key)
-                        Values = monthGroup.GroupByProperty(groupName).OrderBy(g => g.Key)
-                        .Select(g => g.Where(x => x.Regular).Sum(x => x.Hours)).ToArray()
-                    },
-                    new Series("Overtime")
-                    {
-                        Values = monthGroup.GroupByProperty(groupName).OrderBy(g => g.Key)
-                        .Select(g => g.Where(x => x.Overtime).Sum(x => x.Hours)).ToArray()
-                    }
-                };
-
+            this.Series = SeriesGenerator();
         }
+        private List<Series> SeriesGenerator()
+        {
+            var groups = _sourceData
+            .GroupByProperty(_groupName)    // 假設你已經有這個擴充
+            .OrderBy(g => g.Key)
+            .ToList();
 
+            var result = new List<Series>();
+            foreach (var stackSerie in _stackSeries)
+            {
+                // 反射取出 DataItem 的屬性
+                var prop = typeof(T).GetProperty(stackSerie.PropertyName, BindingFlags.Public | BindingFlags.Instance);
+                if (prop == null)
+                    throw new ArgumentException($"找不到屬性 {stackSerie.PropertyName}");
+
+                // 計算每個分群的 Sum(Hours)，條件為 prop.Value == FilterValue
+                var values = groups
+                    .Select(g => g
+                        .Where(item =>
+                        {
+                            var val = prop.GetValue(item);
+                            // 為了安全先判 null，再用 Equals 比對
+                            return val != null && val.Equals(stackSerie.FilterValue);
+                        })
+                        .Sum(x =>
+                        {
+                            // 假設每筆都有 Hours 屬性
+                            var hProp = typeof(T).GetProperty("Hours");
+                            return Convert.ToDouble(hProp.GetValue(x));
+                        }))
+                    .ToArray();
+
+                result.Add(new Series(stackSerie.SeriesName)
+                {
+                    Values = values
+                });
+            }
+            return result;
+        }
     }
-
     public class Series
     {
         public string Name { get; set; }     // 這個系列的名稱（對應 legend）
@@ -49,7 +73,20 @@ namespace ChartAPI.Models
         public Series(string name)
         {
             this.Name = name;
-            //this.Values = doubles;
         }
     }
+    public class StackSeries
+    {
+        public string SeriesName { get; set; }
+        public string PropertyName { get; set; }
+        public object FilterValue { get; set; }
+
+        public StackSeries(string seriesName, string propertyName, object filterValue)
+        {
+            SeriesName = seriesName;
+            PropertyName = propertyName;
+            FilterValue = filterValue;
+        }
+    }
+
 }
