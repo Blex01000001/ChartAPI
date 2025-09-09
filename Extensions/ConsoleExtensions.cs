0bd1a67079e4dcc74eb05f8aa86ebf12ab51506e
DESKTOP <JP4>
2025-09-10 00:11:21 +0800

    update DashboardResponse builder

---
+++ Extensions/ConsoleExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Linq;
+
+namespace ChartAPI.Extensions
+{
+    public static class ConsoleExtensions
+    {
+        public static void WritewithTime(this Console console, string value)
+        {
+            Console.Write($"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}] {value}\n");
+        }
+    }
+}
