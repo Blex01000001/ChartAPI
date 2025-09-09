cef32de00ac60013b1e72bd9b18e0c080ed6b724
Alex <NB285>
2025-09-09 16:31:42 +0800

    create Dashboard class/method

---
+++ DTOs/DashboardResponseDto.cs
@@ -0,0 +1,10 @@
+﻿using ChartAPI.Models;
+
+namespace ChartAPI.DTOs
+{
+    public class DashboardResponseDto
+    {
+        List<MonthlyChartDto> monthlyChartDtos { get; set; }
+        List<StackChartDto<ManHourModel>> stackChartDtos { get; set; }
+    }
+}
