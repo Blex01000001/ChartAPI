4b90e0a271315d915d1772722ed4441caf355aee
Alex <NB285>
2025-09-16 16:17:03 +0800

    Complete UpsertDataByDept

--- ChartBuilders/YearChartBuilder.cs
+++ ChartBuilders/YearChartBuilder.cs
@@ -1,9 +1,14 @@
 ﻿using ChartAPI.DTOs;
+using ChartAPI.Models;
 
 namespace ChartAPI.ChartBuilders
 {
     public class YearChartBuilder
     {
+        public YearChartBuilder(IEnumerable<ManHourModel> manHourList)
+        {
+
+        }
         public YearChartDto Build()
         {
             return new YearChartDto();
